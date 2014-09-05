2af9fdd285ce167e55dc3e5ed36502cb2977c70a
Redian <redian.ibra@gmail.com>
2014-09-05 12:00:20 +0100

    Decalaring the $sharedSecret variable.
    Ditto.

--- docs/examples/confirmation.php
+++ docs/examples/confirmation.php
@@ -35,7 +35,10 @@ session_start();
 Klarna_Checkout_Order::$contentType
     = "application/vnd.klarna.checkout.aggregated-order-v2+json";
 
-$connector = Klarna_Checkout_Connector::create('sharedSecret');
+// Shared secret
+$sharedSecret = 'sharedSecret';
+
+$connector = Klarna_Checkout_Connector::create($sharedSecret);
 
 $checkoutId = $_SESSION['klarna_checkout'];
 $order = new Klarna_Checkout_Order($connector, $checkoutId);
